f28efb355bfc3c3a482ce8d555deafaf5ae76f25
Unknown <emils.gosko@avoin.systems>
2018-06-27 16:33:10 +0300

    11-to-10 backport for project stages
    Backported Project stages to Odoo 10.0

--- project_stage/__manifest__.py
+++ project_stage/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Project Stage',
-    'version': '11.0.1.0.0',
+    'version': '10.0.1.0.0',
     'category': 'Projects & Services',
     'sequence': 14,
     'summary': '',
