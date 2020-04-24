74d20d841f69246f15f524218a58ebec80dc369a
Daniels Andersons <daniels.andersons@avoin.systems>
2020-04-24 08:23:16 +0300

    [MIG] project_stage: removed unnecessary style and field

--- project_stage/models/project_stage.py
+++ project_stage/models/project_stage.py
@@ -20,13 +20,6 @@ class ProjectStage(models.Model):
     )
     sequence = fields.Integer(
     )
-    case_default = fields.Boolean(
-        'Default for New Projects',
-        help="If you check this field, this stage "
-             "will be proposed by default on each "
-             "new project. It will not assign this "
-             "stage to existing projects.",
-    )
     fold = fields.Boolean(
         'Folded in Kanban View',
         help='This stage is folded in the kanban view when'
